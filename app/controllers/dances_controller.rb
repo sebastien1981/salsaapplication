b266c47d236d64228ad63e86c4c76a303035b6eb
sebastien mercier-bellevue <sebastien.mercierbellevue@gmail.com>
2023-11-30 14:34:10 +0100

    dances

---
+++ app/controllers/dances_controller.rb
@@ -0,0 +1,22 @@
+class DancesController < ApplicationController
+  autocomplete :dances, :fullname
+  def new
+    @dance = Dance.new
+  end
+
+  def create
+    @dance = Dance.new(dance_params)
+    if @dance.save
+      redirect_to schools_path, notice: "Vous avez bien crée un nouveau type de dance:#{@dance.fullname}"
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+
+  def dance_params
+      params.require(:dance).permit(:fullname)
+  end
+end
