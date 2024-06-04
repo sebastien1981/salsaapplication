812bd186212c8117f562bbf432cee3ef657c72de
sebastien mercier-bellevue <sebastien.mercierbellevue@gmail.com>
2024-06-04 17:29:14 +0200

    correction de bug mineure

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -19,10 +19,10 @@ class SchoolClassesController < ApplicationController
 
     #recuperation des donnes par ecole en utilisant le case when
   @schoolclasses = SchoolClass.where(school_id: @school.id)
-                               .order(:room_number)
-                               .order(:beginning_of_time)
-                               .order(case_statement)
-                               .order(:day_of_week)
+                              .order(:room_number)
+                              .order(:beginning_of_time)
+                              .order(case_statement)
+                              .order(:day_of_week)
 
     #recuperation des romms dans un array
     roomnumber_arr = []
@@ -127,6 +127,7 @@ class SchoolClassesController < ApplicationController
 
         w += 1
       end#end for each
+
     else
       redirect_to new_school_school_class_path(:school_id => @school.id), notice: "Vous avez été redirigé vers la page pour créer vos planning"
     end
@@ -143,19 +144,20 @@ class SchoolClassesController < ApplicationController
     set_arg_creation
 
     @schoolclass = SchoolClass.new(schoolclass_params)
-    @schoolclass.school = @school
-    @schoolclassesroom = SchoolClass.where(beginning_of_time: @schoolclass.beginning_of_time, end_of_time: @schoolclass.end_of_time, day_of_week: @schoolclass.day_of_week, school_id: @schoolclass.school_id)
-    @schoolclassestea = SchoolClass.where(beginning_of_time: @schoolclass.beginning_of_time, end_of_time: @schoolclass.end_of_time, teacher_name: @schoolclass.teacher_name, day_of_week: @schoolclass.day_of_week)
 
-    if @schoolclassestea.any?
-      redirect_to new_school_school_class_path(:school_id => @school.id), notice: "#{@schoolclass.teacher_name} est déja pris à cette horaire"
-    elsif @schoolclassesroom.any?
-      redirect_to new_school_school_class_path(:school_id => @school.id), notice: "La salle est déja réservé à cette horaire dans cette école"
-    elsif @schoolclass.save
-      redirect_to schools_path, notice: "Votre cours a bien été crée"
-    else
-       render :new, status: :unprocessable_entity
-    end
+        @schoolclass.school = @school
+        @schoolclassesroom = SchoolClass.where(beginning_of_time: @schoolclass.beginning_of_time, end_of_time: @schoolclass.end_of_time, day_of_week: @schoolclass.day_of_week, school_id: @schoolclass.school_id, room_number: @schoolclass.room_number)
+        @schoolclassestea = SchoolClass.where(beginning_of_time: @schoolclass.beginning_of_time, end_of_time: @schoolclass.end_of_time, teacher_name: @schoolclass.teacher_name, day_of_week: @schoolclass.day_of_week)
+
+        if @schoolclassestea.any?
+          redirect_to new_school_school_class_path(:school_id => @school.id), notice: "#{@schoolclass.teacher_name} est déja pris à cette horaire"
+        elsif @schoolclassesroom.any?
+          redirect_to new_school_school_class_path(:school_id => @school.id), notice: "La salle est déja réservé à cette horaire dans cette école"
+        elsif @schoolclass.save
+          redirect_to schools_path, notice: "Votre cours a bien été crée"
+        else
+          render :new, status: :unprocessable_entity
+        end
   end
 
   def destroy
@@ -182,15 +184,15 @@ class SchoolClassesController < ApplicationController
       school = @school.id
     end
     @teacherstoselect = Teacher.joins("INNER JOIN school_teachers ON school_teachers.teacher_id = teachers.id INNER JOIN schools ON schools.id = school_teachers.school_id where schools.id = #{school} or schools.school_id = #{school} ")
-     #nombre de room
-     x = @school.number_room
-     arrtess = []#pour stocker les valeurs que je souhaite
-     arrtess << x
-     while x > 1
+    #nombre de room
+    x = @school.number_room
+    arrtess = []#pour stocker les valeurs que je souhaite
+    arrtess << x
+    while x > 1
       x -= 1
       arrtess << x
-     end
-     @countarr = arrtess
+    end
+    @countarr = arrtess
 
   end
 end
